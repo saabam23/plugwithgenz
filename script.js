99e0276a4dfe7cd742a45a05024ca4086a4a9312
Sydney Abam <106718254+saabam23@users.noreply.github.com>
2024-05-15 01:06:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,182 @@
+// TERM PAGE: toggling each term
+function toggleClin(){
+  var t1 = document.getElementById("t1");
+  if (t1.style.display === "none") {
+    t1.style.display = "block";
+  } else {
+    t1.style.display = "none";
+  }
+}
+
+function togglePer(){
+  var t2 = document.getElementById("t2");
+  if (t2.style.display === "none") {
+    t2.style.display = "block";
+  } else {
+    t2.style.display = "none";
+  }
+}
+
+function togglePost(){
+  var t3 = document.getElementById("t3");
+  if (t3.style.display === "none") {
+    t3.style.display = "block";
+  } else {
+    t3.style.display = "none";
+  }
+}
+
+function togglePan(){
+  var t4 = document.getElementById("t4");
+  if (t4.style.display === "none") {
+    t4.style.display = "block";
+  } else {
+    t4.style.display = "none";
+  }
+}
+
+function toggleSea(){
+  var t5 = document.getElementById("t5");
+  if (t5.style.display === "none") {
+    t5.style.display = "block";
+  } else {
+    t5.style.display = "none";
+  }
+}
+
+function toggleOCD(){
+  var t6 = document.getElementById("t6");
+  if (t6.style.display === "none") {
+    t6.style.display = "block";
+  } else {
+    t6.style.display = "none";
+  }
+}
+
+function togglePTSD(){
+  var t7 = document.getElementById("t7");
+  if (t7.style.display === "none") {
+    t7.style.display = "block";
+  } else {
+    t7.style.display = "none";
+  }
+}
+
+function toggleBP(){
+  var t8 = document.getElementById("t8");
+  if (t8.style.display === "none") {
+    t8.style.display = "block";
+  } else {
+    t8.style.display = "none";
+  }
+}
+
+function toggleADHD(){
+  var t9 = document.getElementById("t9");
+  if (t9.style.display === "none") {
+    t9.style.display = "block";
+  } else {
+    t9.style.display = "none";
+  }
+}
+
+function toggleBD(){
+  var t10 = document.getElementById("t10");
+  if (t10.style.display === "none") {
+    t10.style.display = "block";
+  } else {
+    t10.style.display = "none";
+  }
+}
+
+function toggleDD(){
+  var t11 = document.getElementById("t11");
+  if (t11.style.display === "none") {
+    t11.style.display = "block";
+  } else {
+    t11.style.display = "none";
+  }
+}
+
+function toggleBPD(){
+  var t12 = document.getElementById("t12");
+  if (t12.style.display === "none") {
+    t12.style.display = "block";
+  } else {
+    t12.style.display = "none";
+  }
+}
+
+function toggleP(){
+  var t13 = document.getElementById("t13");
+  if (t13.style.display === "none") {
+    t13.style.display = "block";
+  } else {
+    t13.style.display = "none";
+  }
+}
+
+function toggleSP(){
+  var t14 = document.getElementById("t14");
+  if (t14.style.display === "none") {
+    t14.style.display = "block";
+  } else {
+    t14.style.display = "none";
+  }
+}
+
+function toggleSc(){
+  var t15 = document.getElementById("t15");
+  if (t15.style.display === "none") {
+    t15.style.display = "block";
+  } else {
+    t15.style.display = "none";
+  }
+}
+
+function toggleSa(){
+  var t16 = document.getElementById("t16");
+  if (t16.style.display === "none") {
+    t16.style.display = "block";
+  } else {
+    t16.style.display = "none";
+  }
+}
+
+function toggleLS(){
+  var t17 = document.getElementById("t17");
+  if (t17.style.display === "none") {
+    t17.style.display = "block";
+  } else {
+    t17.style.display = "none";
+  }
+}
+
+function toggleT(){
+  var t18 = document.getElementById("t18");
+  if (t18.style.display === "none") {
+    t18.style.display = "block";
+  } else {
+    t18.style.display = "none";
+  }
+}
+
+function togglePho(){
+  var t19 = document.getElementById("t19");
+  if (t19.style.display === "none") {
+    t19.style.display = "block";
+  } else {
+    t19.style.display = "none";
+  }
+}
+
+function toggleED(){
+  var t20 = document.getElementById("t20");
+  if (t20.style.display === "none") {
+    t20.style.display = "block";
+  } else {
+    t20.style.display = "none";
+  }
+}
+var button = $('button');
+button.on ("click,")
